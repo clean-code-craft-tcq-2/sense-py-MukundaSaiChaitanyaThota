0a607bac88f3af61a47f1ca883abe7e2c9e6d99f
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2021-10-21 18:54:17 +0530

    Update statistics.py
    added function body

--- statistics.py
+++ statistics.py
@@ -1,3 +1,12 @@
+import numpy as np
 
 def calculateStats(numbers):
-  return None
+    if len(numbers) != 0:
+        avg = round(sum(numbers)/len(numbers),3)
+        max1 = max(numbers)
+        min1 = min(numbers)
+        stats = {"avg":avg,"max":max1,"min":min1}
+    
+    else:
+        stats = {"avg":np.nan,"max":np.nan,"min":np.nan}
+    return stats
