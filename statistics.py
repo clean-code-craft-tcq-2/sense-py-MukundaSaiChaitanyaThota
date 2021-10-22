6d8cb116bb389c636b9e585a508c638ac7678832
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2021-10-22 19:51:23 +0530

    Update statistics.py
    Updated classes

--- statistics.py
+++ statistics.py
@@ -10,3 +10,23 @@ def calculateStats(numbers):
         stats = {"avg":float("NaN"),"max":float("NaN"),"min":float("NaN")}
         
     return stats
+
+class EmailAlert():
+    def __init__(self):
+        self.emailSent = 0
+
+class LEDAlert():
+    def __init__(self):
+        self.ledGlows = 0
+        
+class StatsAlerter():
+    def __init__(self, maxThreshold, Alerts):
+        self.maxThreshold = maxThreshold
+        self.Alerts = Alerts
+    def checkAndAlert(self, numbers):
+        if max(numbers) > self.maxThreshold:
+            self.Alerts[0].emailSent = 1
+            self.Alerts[1].ledGlows = 1
+        else:
+            self.Alerts[0].emailSent = 0
+            self.Alerts[1].ledGlows = 0
